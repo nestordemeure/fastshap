9396e3285537ac95230e8aa5512cb4f6de9f4d79
Zachary Mueller <muellerzr@gmail.com>
2020-02-10 08:29:07 -0600

    fix bugs

--- fastshap/interp.py
+++ fastshap/interp.py
@@ -10,27 +10,27 @@ from fastai2.tabular.all import *
 # Cell
 class ShapInterpretation():
   "Base interpereter to use the `SHAP` interpretation library"
-  def __init__(self, learn:TabularLearner, test_data=None, link='identity', l1_reg='auto', matplotlib=False, **kwargs):
+  def __init__(self, learn:TabularLearner, test_data=None, link='identity', l1_reg='auto', matplotlib=False, n_samples=128, **kwargs):
     "Initialize `ShapInterpretation` with a Learner, test_data, link, `n_samples`, `l1_reg`, and optional **kwargs"
     self.model = learn.model
     self.dls = learn.dls
     self.class_names = learn.dl.vocab
-    self.train_data = learn.dls.all_cols
-    self.test_data = _prepare_data(learn, test_data)
+    self.train_data = pd.merge(learn.dls.cats, learn.dls.conts, left_index=True, right_index=True)
+    self.test_data = _prepare_data(learn, test_data, n_samples)
     self.is_mat = matplotlib
     pred_func = partial(_predict, learn)
     self.explainer = shap.SamplingExplainer(pred_func, self.train_data, **kwargs)
     self.shap_vals = self.explainer.shap_values(self.test_data, l1_reg=l1_reg)
     self.is_multi_output = isinstance(self.shap_vals, list)
 
-  def decision_plot(self, class_id=0, idx=-1, **kwargs):
+  def decision_plot(self, class_id=0, row_idx=-1, **kwargs):
     "Visualize model decision using cumulative `SHAP` values."
     shap_vals, exp_val = _get_values(self, class_id)
-    if idx == -1:
+    if row_idx == -1:
       return shap.decision_plot(exp_val, shap_vals[:10], self.test_data.iloc[:10], **kwargs)
-    return shap.decision_plot(exp_val, shap_vals[idx], self.test_data.iloc[idx], **kwargs)
+    return shap.decision_plot(exp_val, shap_vals[row_idx], self.test_data.iloc[row_idx], **kwargs)
 
-  def dependence_plot(self, variable_name:str="", class_id=0, idx:int=0, **kwargs):
+  def dependence_plot(self, variable_name:str="", class_id=0, **kwargs):
     "Plots value of variable on the x-axis and the SHAP value of the same variable on the y-axis"
     if variable_name is "":
       raise ValueError('No variable passed in for `variable_name`')
@@ -51,7 +51,7 @@ class ShapInterpretation():
     "Plots explaination of single prediction as waterfall plot"
     shap_vals, exp_val = _get_values(self, class_id)
     n_rows = shap_vals.shape[0]
-    row_idx = random.randint(0, n_rows-1) if row_idx is not None else row_idx
+    row_idx = random.randint(0, n_rows-1) if row_idx is None else row_idx
     print(f'Displaying row {row_idx} of {n_rows} (use `row_idx` to specify another row)')
     feat_names = self.test_data.columns
     return shap.waterfall_plot(exp_val, shap_vals[row_idx,:], feature_names=feat_names, **kwargs)
@@ -62,3 +62,15 @@ def _get_class_info(interp:ShapInterpretation, class_id):
   if isinstance(class_id, int): class_idx, class_name = class_id, interp.class_names[class_id]
   else: class_idx, class_name = interp.class_names.o2i[class_id], class_id
   return (class_name, class_idx)
+
+# Cell
+def _get_values(interp:ShapInterpretation, class_id=0):
+  "Returns `shap_value` and `expected_value`"
+  shap_vals = interp.shap_vals
+  exp_vals = interp.explainer.expected_value
+  if interp.is_multi_output:
+    (class_name, class_idx) = _get_class_info(interp, class_id)
+    print(f"Classification model detected, displaying score for the class {class_name}.")
+    print("(use `class_id` to specify another class)")
+    return (shap_vals[class_idx], exp_vals[class_idx])
+  return (shap_vals, exp_vals)
